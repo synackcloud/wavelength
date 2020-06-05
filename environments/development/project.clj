6f970b5b84c794be72a47bcac77e8e3c4132cfe0
Alec Goebel <alec.goebel@slalom.com>
2020-06-05 17:01:23 -0400

    Initial commit.

---
+++ environments/development/project.clj
@@ -0,0 +1,3 @@
+(defproject synack.wavelength/development "1.0"
+  :description "The main development environment."
+  :dependencies [[org.clojure/clojure "1.9.0"]])
---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject synack.wavelength/wavelength "1.0"
+  :description "A Polylith workspace."
+  :plugins [[polylith/lein-polylith "0.2.3"]]
+  :polylith {:top-namespace "synack.wavelength"
+             :clojure-version "1.9.0"})
